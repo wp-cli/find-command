466b4aa3ae78717afec99b991403afacba3b999d
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-21 06:48:01 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+danielbachhuber/finder-command
+==============================
+
+
+
+[![Build Status](https://travis-ci.org/danielbachhuber/finder-command.svg?branch=master)](https://travis-ci.org/danielbachhuber/finder-command)
+
+Quick links: [Using](#using) | [Installing](#installing) | [Contributing](#contributing)
+
+## Using
+
+~~~
+wp find-wp <path> [--depth=<depth>] [--skip-ignored-paths] [--field=<field>] [--format=<format>] [--verbose]
+~~~
+
+Recursively iterates subdirectories of provided `<path>` to find and
+report WordPress installs. A WordPress install is a wp-includes directory
+with a version.php file.
+
+Avoids recursing some known paths (e.g. node_modules) to significantly
+improve performance.
+
+**OPTIONS**
+
+	<path>
+		Path to search the subdirectories of.
+
+	[--depth=<depth>]
+		Limit recursion to a specific depth.
+
+	[--skip-ignored-paths]
+		Skip the paths that are ignored by default.
+
+	[--field=<field>]
+		Output a specific field.
+
+	[--format=<format>]
+		Render output in a specific format.
+		---
+		default: table
+		options:
+		  - table
+		  - json
+		  - csv
+		  - yaml
+		---
+
+	[--verbose]
+		Log useful information to STDOUT.
+
+## Installing
+
+Installing this package requires WP-CLI v0.23.0 or greater. Update to the latest stable release with `wp cli update`.
+
+Once you've done so, you can install this package with `wp package install danielbachhuber/finder-command`.
+
+## Contributing
+
+We appreciate you taking the initiative to contribute to this project.
+
+Contributing isn’t limited to just code. We encourage you to contribute in the way that best fits your abilities, by writing tutorials, giving a demo at your local meetup, helping other users with their support questions, or revising our documentation.
+
+### Reporting a bug
+
+Think you’ve found a bug? We’d love for you to help us get it fixed.
+
+Before you create a new issue, you should [search existing issues](https://github.com/danielbachhuber/finder-command/issues?q=label%3Abug%20) to see if there’s an existing resolution to it, or if it’s already been fixed in a newer version.
+
+Once you’ve done a bit of searching and discovered there isn’t an open or fixed issue for your bug, please [create a new issue](https://github.com/danielbachhuber/finder-command/issues/new) with the following:
+
+1. What you were doing (e.g. "When I run `wp post list`").
+2. What you saw (e.g. "I see a fatal about a class being undefined.").
+3. What you expected to see (e.g. "I expected to see the list of posts.")
+
+Include as much detail as you can, and clear steps to reproduce if possible.
+
+### Creating a pull request
+
+Want to contribute a new feature? Please first [open a new issue](https://github.com/danielbachhuber/finder-command/issues/new) to discuss whether the feature is a good fit for the project.
+
+Once you've decided to commit the time to seeing your pull request through, please follow our guidelines for creating a pull request to make sure it's a pleasant experience:
+
+1. Create a feature branch for each contribution.
+2. Submit your pull request early for feedback.
+3. Include functional tests with your changes. [Read the WP-CLI documentation](https://wp-cli.org/docs/pull-requests/#functional-tests) for an introduction.
+4. Follow the [WordPress Coding Standards](http://make.wordpress.org/core/handbook/coding-standards/).
+
+
+*This README.md is generated dynamically from the project's codebase using `wp scaffold package-readme` ([doc](https://github.com/wp-cli/scaffold-package-command#wp-scaffold-package-readme)). To suggest changes, please submit a pull request against the corresponding part of the codebase.*
---
+++ command.php
@@ -0,0 +1,160 @@
+<?php
+
+if ( ! class_exists( 'WP_CLI' ) ) {
+	return;
+}
+
+use WP_CLI\Utils;
+
+class Finder_Command {
+
+	/**
+	 * Paths we can probably ignore recursion into.
+	 *
+	 * @var array
+	 */
+	private $ignored_paths = array(
+		'/.git/',
+		'/.svn/',
+		'/wp-admin/',
+		'/wp-content/',
+		'/node_modules/',
+	);
+
+	/**
+	 * Whether or not to skip ignored paths.
+	 *
+	 * @var bool
+	 */
+	private $skip_ignored_paths = false;
+
+	/**
+	 * Whether or not to be verbose with informational output.
+	 *
+	 * @var bool
+	 */
+	private $verbose = false;
+
+	/**
+	 * Found WordPress installs.
+	 *
+	 * @var array
+	 */
+	private $found_wp = array();
+
+	/**
+	 * Finds WordPress installs on the filesystem.
+	 *
+	 * Recursively iterates subdirectories of provided `<path>` to find and
+	 * report WordPress installs. A WordPress install is a wp-includes directory
+	 * with a version.php file.
+	 *
+	 * Avoids recursing some known paths (e.g. node_modules) to significantly
+	 * improve performance.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <path>
+	 * : Path to search the subdirectories of.
+	 *
+	 * [--depth=<depth>]
+	 * : Limit recursion to a specific depth.
+	 *
+	 * [--skip-ignored-paths]
+	 * : Skip the paths that are ignored by default.
+	 *
+	 * [--field=<field>]
+	 * : Output a specific field.
+	 *
+	 * [--format=<format>]
+	 * : Render output in a specific format.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - json
+	 *   - csv
+	 *   - yaml
+	 * ---
+	 *
+	 * [--verbose]
+	 * : Log useful information to STDOUT.
+	 *
+	 * @when before_wp_load
+	 */
+	public function __invoke( $args, $assoc_args ) {
+		list( $path ) = $args;
+		$path = realpath( $path );
+		$this->skip_ignored_paths = Utils\get_flag_value( $assoc_args, 'skip-ignored-paths' );
+		$this->verbose = Utils\get_flag_value( $assoc_args, 'verbose' );
+		$this->log( "Searching for WordPress installs in {$path}" );
+		$this->recurse_directory( $path );
+		$formatter = new \WP_CLI\Formatter( $assoc_args, array( 'version_path', 'version' ) );
+		$formatter->display_items( $this->found_wp );
+	}
+
+	private function recurse_directory( $path ) {
+
+		// Assume this symlink will be traversed from its true direction
+		if ( is_link( $path ) ) {
+			return;
+		}
+
+		// Provide consistent trailing slashes to all paths
+		$path = rtrim( $path, '/' ) . '/';
+
+		// Don't recurse directories that probably don't have a WordPress install.
+		if ( ! $this->skip_ignored_paths ) {
+			foreach( $this->ignored_paths as $ignored_path ) {
+				if ( false !== stripos( $path, $ignored_path ) ) {
+					$this->log( "Matched ignored path. Skipping recursion into {$path}" );
+					return;
+				}
+			}
+		}
+
+		// This looks like a wp-includes directory, so check if it has a
+		// version.php file.
+		if ( '/wp-includes/' === substr( $path, -13 )
+			&& file_exists( $path . 'version.php' ) ) {
+			$version_path = $path . 'version.php';
+			$this->found_wp[ $version_path ] = array(
+				'version_path' => $version_path,
+				'version'      => self::get_wp_version( $version_path ),
+			);
+			$this->log( "Found WordPress install at {$version_path}" );
+			return;
+		}
+
+		// Check all files and directories of this path to recurse
+		// into subdirectories.
+		$this->log( "Recusing into {$path}" );
+		$iterator = new RecursiveDirectoryIterator( $path, FilesystemIterator::SKIP_DOTS );
+		foreach( $iterator as $file_info ) {
+			if ( $file_info->isDir() ) {
+				$this->recurse_directory( $file_info->getPathname() );
+			}
+		}
+	}
+
+	/**
+	 * Get the WordPress version for the install, without executing the file.
+	 */
+	private static function get_wp_version( $path ) {
+		$contents = file_get_contents( $path );
+		preg_match( '#\$wp_version\s?=\s?[\'"]([^\'"]+)[\'"]#' , $contents, $matches );
+		return ! empty( $matches[1] ) ? $matches[1] : '';
+	}
+
+	/**
+	 * Log informational message to STDOUT depending on verbosity.
+	 */
+	private function log( $message ) {
+		if ( $this->verbose ) {
+			WP_CLI::log( $message );
+		}
+	}
+
+}
+WP_CLI::add_command( 'find-wp', 'Finder_Command' );
+
